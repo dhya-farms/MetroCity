3707f62cdb4e18b182e707dd0cdc8b52b3a72708
dhya-farms <sandbox@dhya.in>
2024-03-07 17:48:13 +0530

    updated to Https redirect

--- config/settings/production.py
+++ config/settings/production.py
@@ -42,13 +42,11 @@ CACHES = {
 # SECURITY
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
-# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
-# SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=False)
-# # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
-# SESSION_COOKIE_SECURE = False
-# # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
-# CSRF_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=False)
+CSRF_COOKIE_SECURE = env.bool("CSRF_COOKIE_SECURE", False)
+SESSION_COOKIE_SECURE = env.bool("SESSION_COOKIE_SECURE", False)
 # # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
